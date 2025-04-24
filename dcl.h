4d7633bae65443123b06d2f734f199bfc2e298e8
LukE-fly <90730805+LukE-fly@users.noreply.github.com>
2025-04-24 11:10:05 -0400

    Create dcl.h

---
+++ dcl.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdint.h>
+#include <stdio.h>
+
+
+/*#define sspint int16_t
+#define spint uint16_t
+#define udpint uint32_t
+#define dpint uint32_t*/
+
+#define sspint int64_t
+#define spint uint64_t
+#define udpint __uint128_t
+#define dpint __uint128_t
+
+void sparse_matrix_multiply_HLS(spint x[20], spint y[20], spint z[20]);
+
+
