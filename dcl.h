219f6baacee1ef9adf9878feda9e4682204a1f2c
Gabriel Lopez Lugo <glugo3@gatech.edu>
2025-04-29 19:33:11 -0400

    Overhauled dcl.h to encompass all three data types

--- dcl.h
+++ dcl.h
@@ -1,18 +1,49 @@
 #include <iostream>
-#include <stdint.h>
+#include <cstdint>
 #include <stdio.h>
 
+#ifdef BIT16
+using sspint = int16_t;
+using spint = uint16_t;
+using udpint = uint32_t;
+using dpint = uint32_t;	
 
-/*#define sspint int16_t
-#define spint uint16_t
-#define udpint uint32_t
-#define dpint uint32_t*/
+constexpr spint Wordlength = 16;
+constexpr spint Nlimbs = 20;
+constexpr spint Radix = 13;
+constexpr spint Nbits = 255;
+constexpr spint Nbytes = 32;
 
-#define sspint int64_t
-#define spint uint64_t
-#define udpint __uint128_t
-#define dpint __uint128_t
+#elif BIT32
+using sspint = int32_t;
+using spint = uint32_t;
+using udpint = uint64_t;
+using dpint = uint64_t;
+
+constexpr spint Wordlength = 32;
+constexpr spint Nlimbs = 9;
+constexpr spint Radix = 29;
+constexpr spint Nbits = 255;
+constexpr spint Nbytes = 32;
+
+#else
+using sspint = int64_t;
+using spint = uint64_t;
+using udpint = __uint128_t;
+using dpint = __uint128_t;
+
+constexpr spint Wordlength = 64;
+constexpr spint Nlimbs = 5;
+constexpr spint Radix = 51;
+constexpr spint Nbits = 255;
+constexpr spint Nbytes = 32;
+#endif
+
+constexpr spint OverflowBits = Radix * Nlimbs - Nbits;
+constexpr spint FinalLimbRadix = Radix - OverflowBits; 
+constexpr spint LastLimbOverflows = Nbits < Radix * Nlimbs;
+
+constexpr spint CongruentFactor = 0x13;
+constexpr spint ReductionFactor = CongruentFactor * (1 << OverflowBits);
 
 void mod_mul(spint x[20], spint y[20], spint z[20]);
-
-
