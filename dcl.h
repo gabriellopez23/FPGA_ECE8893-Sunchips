1361619d31f0d69354a9a7ddc857a0da66f9bd08
Gabriel Lopez Lugo <glugo3@gatech.edu>
2025-05-02 16:34:05 -0400

    Massive changes to the implementation

--- dcl.h
+++ dcl.h
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cmath>
 #include <iostream>
 #include <cstdint>
@@ -6,37 +7,38 @@
 #include <hls_vector.h>
 
 #ifdef BIT16
-using sspint = int16_t;
 using spint = uint16_t;
 using udpint = uint32_t;
 using dpint = uint32_t;	
 
 constexpr spint Wordlength = 16;
-constexpr spint Nlimbs = 20;
 constexpr spint Radix = 13;
 
 #elif BIT32
-using sspint = int32_t;
 using spint = uint32_t;
 using udpint = uint64_t;
 using dpint = uint64_t;
 
 constexpr spint Wordlength = 32;
-constexpr spint Nlimbs = 9;
 constexpr spint Radix = 29;
 
 #else
-using sspint = int64_t;
-using spint = uint64_t;
-using udpint = __uint128_t;
-using dpint = __uint128_t;
+// using spint = uint64_t;
+// using udpint = __uint128_t;
+// using dpint = __uint128_t;
 
-constexpr spint Wordlength = 64;
-constexpr spint Nlimbs = 5;
-constexpr spint Radix = 51;
+constexpr uint32_t Wordlength = 64;
+constexpr uint32_t Radix = 13;
+// using spint = uint32_t;
+// using udpint = uint64_t;
+// using dpint = uint64_t;
+
+// constexpr uint32_t Wordlength = 32;
+// constexpr uint32_t Radix = 29;
+
+// constexpr uint32_t Wordlength = 16;
+// constexpr uint32_t Radix = 13;
 #endif
-
-using spint_vec = hls::vector<spint, Nlimbs>;
 
 // Only way to get number of bits at compile time in C++14
 static constexpr unsigned numberOfBits(unsigned x)
@@ -44,19 +46,29 @@ static constexpr unsigned numberOfBits(unsigned x)
     return x < 2 ? x : 1 + numberOfBits(x >> 1);
 }
 
+// Only way to use ceiling division at compile time in C++14
+template<typename T>
+static constexpr T ceil(T num, T den) {
+    return (num + den - 1) / den;
+}
 
+constexpr uint32_t Nbytes = 32;
+constexpr uint32_t Nbits = 255;
+constexpr uint32_t Nlimbs = ceil(Nbits, Radix);
+constexpr uint32_t OverflowBits = Radix * Nlimbs - Nbits;
+constexpr uint32_t FinalLimbRadix = Radix - OverflowBits; 
+constexpr bool LastLimbOverflows = Nbits < Radix * Nlimbs;
 
-constexpr spint Nbits = 255;
-constexpr spint Nbytes = 32;
-constexpr spint OverflowBits = Radix * Nlimbs - Nbits;
-constexpr spint FinalLimbRadix = Radix - OverflowBits; 
-constexpr spint LastLimbOverflows = Nbits < Radix * Nlimbs;
-
-constexpr dpint CongruentFactor = 0x13; // 19
-constexpr dpint ReductionFactor = CongruentFactor * (1 << OverflowBits);
+constexpr uint32_t CongruentFactor = 0x13; // 19 in decimal
+constexpr uint32_t ReductionFactor = CongruentFactor * (1 << OverflowBits);
 
 // Calculates the number of bits to avoid overflow 
-constexpr spint TermWidth = 2 * Radix + numberOfBits(ReductionFactor) + numberOfBits(Nlimbs - 1) + 1;
-using somethign = ap_uint<TermWidth>; 
+constexpr uint32_t TermWidth = 2 * Radix + numberOfBits(ReductionFactor) + numberOfBits(Nlimbs - 1) + 1;
+
+using spint = ap_uint<Radix>; 
+using dpint = ap_uint<TermWidth>; 
+using udpint = ap_uint<TermWidth>; 
+using spint_vec = hls::vector<spint, Nlimbs>;
+using spint_red_vec = hls::vector<ap_uint<Radix + numberOfBits(ReductionFactor) + 1>, Nlimbs>; 
 
 void mod_mul(spint_vec& x, spint_vec& y, spint_vec& z);
--- top.cpp
+++ top.cpp
@@ -1,82 +1,76 @@
 #include "dcl.h"
 
-void mod_mul(spint_vec& x, spint_vec& y, spint_vec& z) {
+void get_reduced_terms(spint_vec& src, spint_red_vec &dst)
+{
+  for (int i = 1; i < Nlimbs; i++) {
+#pragma HLS unroll
+    dst[i] = src[i] * ReductionFactor;
+  }
+}
+
+void mod_mul(spint_vec &x, spint_vec &y, spint_vec &z) {
 #pragma HLS interface mode = axis port = x // offset = slave bundle = mem1
 #pragma HLS interface mode = axis port = y // offset = slave bundle = mem2
 #pragma HLS interface mode = axis port = z // offset = slave bundle = mem3
 #pragma HLS interface s_axilite port = return
 
   dpint t = 0;
-  // spint lo = 0;
-  // spint hi = 0;
   spint mask = ((spint)1 << Radix) - (spint)1;
-
-  spint_vec v;
-#pragma HLS array_partition variable = v complete
 
   spint_vec x_buf = x;
   spint_vec y_buf = y;
-#pragma HLS array_partition variable = x_buf complete
-#pragma HLS array_partition variable = y_buf complete
+  spint_vec z_buf;
+  
+  spint_vec v;
+  spint_red_vec x_buf_reduced;
+
+  get_reduced_terms(x_buf, x_buf_reduced);
 
 REDUCTION:
   for (int i = 0; i < Nlimbs; i++) {
 #pragma HLS unroll
+    dpint tt = 0;
 
-    dpint tt[Nlimbs];
-#pragma HLS array_partition variable = tt complete
-
-  // Calculate high terms (terms that are reduced modulo 2^255 - 19)
+    // Calculate high terms (terms that are reduced modulo 2^255 - 19)
   HIGH_TERMS:
     for (int j = 1 + i; j < Nlimbs; j++) {
-#pragma HLS unroll factor = (Nlimbs - 1)
-      tt[j] = (dpint)x_buf[j] * (dpint)y_buf[Nlimbs - j + i];
+#pragma HLS unroll
+      tt += x_buf_reduced[j] * y_buf[Nlimbs - j + i];
     }
-
-    // lo = (spint)tt & mask;
-    // t += (dpint)(lo + hi) * ReductionFactor;
-
-  SUM:
-    for (int j = 1 + i; j < Nlimbs; j++) {
-#pragma HLS unroll factor = (Nlimbs - 1)
-      t += (dpint)tt[j] * (dpint)ReductionFactor;
-    }
-  // t += tt * ReductionFactor;
-  // hi = (spint)(tt >> Radix);
 
   // Calculate direct terms (terms that aren't reduced)
   DIRECT_TERMS:
     for (int j = 0; j < i + 1; j++) {
-#pragma HLS unroll factor = Nlimbs
-      t += (dpint)x_buf[j] * (dpint)y_buf[i - j];
+#pragma HLS unroll
+      t += x_buf[j] * y_buf[i - j];
     }
 
-    v[i] = (spint)t & mask;
+    // Add the high terms to the direct terms
+    t += tt;
+
+    v[i] = t & mask;
     t = t >> Radix;
   }
 
   // Second reduction pass
-  udpint ut = (udpint)t;
+  udpint ut = t;
 
   // Reduce the overflow bits of the last term
-  if (Nbits < Radix * Nlimbs) {
-    ut = (udpint)(ut << OverflowBits) +
+  if (LastLimbOverflows) {
+    ut = (dpint)(ut << OverflowBits) +
          (spint)(v[Nlimbs - 1] >> (FinalLimbRadix));
 
     // Get the non-overflow bits
-    v[Nlimbs - 1] &= ((sspint)1 << FinalLimbRadix) - 1;
+    v[Nlimbs - 1] &= (spint(1) << FinalLimbRadix) - 1;
   }
-
   // Reduce the overflow bits of the last term
   ut *= CongruentFactor;
 
   // Add the reduced bits to the first term
-  spint sum = v[0] + ((spint)ut & mask);
+  spint sum = v[0] + (ut & mask);
 
-  spint_vec z_buf;
-#pragma HLS array_partition variable = z_buf complete
 
-  z_buf[0] = (spint)(sum & mask);
+  z_buf[0] = sum & mask;
 
   // Get the carry bits from the sum and the overflow bits
   spint carry = (sum >> Radix) + (spint)(ut >> Radix);
@@ -90,5 +84,6 @@ SET_LIMBS:
     z_buf[i] = v[i];
   }
 
+  // Send the result to the output
   z = z_buf;
 }
